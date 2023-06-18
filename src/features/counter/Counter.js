a0c5d57d9b352b97c057c9643fd4f449cb7644f4
shivarama <saanand@protonmail.com>
2023-06-18 23:22:42 +0530

    initial commit

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from './counterSlice';
+// import styles from './Counter.module.css';
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <d>
+        <button
+          aria-label='Decrement value'
+          onClick={() => dispatch(decrement())}>
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label='Increment value'
+          onClick={() => dispatch(increment())}>
+          +
+        </button>
+      </d>
+      <div>
+        <input
+          aria-label='Set increment amount'
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>
+          Add Amount
+        </button>
+        <button onClick={() => dispatch(incrementAsync(incrementValue))}>
+          Add Async
+        </button>
+        <button onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+          Add If Odd
+        </button>
+      </div>
+    </div>
+  );
+}
